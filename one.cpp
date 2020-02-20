4d2171e50dfee7b319299e1da0b5a24f89432953
Sophie Crowley <crowleso@tcd.ie>
2020-02-20 20:53:38 +0100

    couting

--- one.cpp
+++ one.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
+int numberOfUsedLibraries = 0;
+
 int main() {
     int libs[2];
+    int usedLibraries[sizeof(libs)/sizeof(*libs)];
     int scansPerDay[] = {2,1};
     int signUp[] = {2,3};
     int days = 7;
@@ -10,6 +13,7 @@ int main() {
     double avgScan = 1.5;
     int libBooks[2][5] = {{4,3,2,1,1}, {5,3,2,0}};
     for(int i=0; i < 2; i++) {
+        days = 7;
         int numberOfBooks = (days-signUp[i]) * scansPerDay[i];
         int ourBooks[numberOfBooks];
         int points = 0;
@@ -17,6 +21,10 @@ int main() {
             ourBooks[j] = libBooks[i][j];
             points+= ourBooks[j];
             libBooks[i][j] = 0;
+        }
+        if(points==0) {
+            libs[i] = INT_MIN;
+            break;
         }
         points*=scansPerDay[i];
         points -= (avgScore * avgScan) * signUp[i];
@@ -33,4 +41,25 @@ int main() {
             }
         }
     }
+    days = 7;
+    for(int i = 0; i < sizeof(libs)/sizeof(*libs) && days>0; i++) {
+        int currLibScore = -10000;
+        int index = -1;
+        for(int j=0; j < sizeof(libs)/sizeof(*libs); j++) {
+            if (libs[j]  > currLibScore) {
+                currLibScore = libs[j];
+                index = j;
+            }
+        }
+        libs[index] = -10000;
+        usedLibraries[i] = index;
+        days -= signUp[index];
+        numberOfUsedLibraries++;
+    }
+
+    cout << numberOfUsedLibraries << endl;
+    for(int i=0; i < numberOfUsedLibraries; i++) {
+        cout << usedLibraries[i] << " ";
+    }
+    cout << endl;
 }
